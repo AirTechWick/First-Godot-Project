8f8fdb1c81aa9993a58b74d8bea75fadbd25be25
Air <erikbolt11@gmail.com>
2022-09-03 13:24:28 -0700

    enemy kinda works

--- BeeEnemyScript.gd
+++ BeeEnemyScript.gd
@@ -4,15 +4,16 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+const UPWARDS = Vector2(0,-1)
+onready var _animated_sprite = $AnimatedSprite
+onready var _bee_timer = $BeeTimer
 var velocity = Vector2()
 export var flying_speed = 200
 export var flying_range = 10  
-onready var _animated_sprite = $AnimatedSprite
-onready var _bee_timer = $BeeTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity.x = flying_speed # normalizing it?
+	velocity.x = flying_speed
 	_bee_timer.wait_time = flying_range/10 # Changes the wait time of the change direction timer
 
 
@@ -21,13 +22,20 @@ func _ready():
 func _process(delta):
 	# -1 in the Vector2 means that UP direction is towards -1 y coordinate
 	# We are changing velocity by velocity.x everytime move and slide is called
-	velocity = move_and_slide(velocity, Vector2(0, -1)) 
+	 # normalizing it?
+	velocity = move_and_slide(velocity, UPWARDS) 
 
 
 
 func changeDirection():
-	if _animated_sprite.flip_h == true:
+	velocity.x *= -1
+	
+	if velocity.x > 0:
 		_animated_sprite.flip_h = false
-	else:
+	elif velocity.x < 0:
 		_animated_sprite.flip_h = true
-	velocity.x *= -1 
+	else: # if velocity is 0
+		if _animated_sprite.flip_h == false: # sprite facing right
+			velocity.x = flying_speed
+		else:
+			velocity.x = -flying_speed
