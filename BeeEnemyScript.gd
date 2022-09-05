2c02171ff4ed83e533fcde0b058da01181a8a928
erikr <erikbolt11@gmail.com>
2022-09-05 16:15:05 -0700

    added bee hit animation and player bounces up when jumping on bee

--- BeeEnemyScript.gd
+++ BeeEnemyScript.gd
@@ -6,7 +6,8 @@ extends KinematicBody2D
 # var b = "text"
 const UPWARDS = Vector2(0,-1)
 onready var _animated_sprite = $AnimatedSprite
-onready var _bee_timer = $BeeTimer
+onready var _bee_timer = $BeeDirectionTimer
+onready var _bee_animation_timer = $BeeHitAnimationTimer
 var velocity = Vector2()
 export var flying_speed = 200
 export var flying_range = 10  
@@ -16,15 +17,12 @@ func _ready():
 	velocity.x = flying_speed
 	_bee_timer.wait_time = flying_range/10 # Changes the wait time of the change direction timer
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# -1 in the Vector2 means that UP direction is towards -1 y coordinate
 	# We are changing velocity by velocity.x everytime move and slide is called
 	 # normalizing it?
 	velocity = move_and_slide(velocity, UPWARDS) 
-
 
 
 func changeDirection():
@@ -39,15 +37,14 @@ func changeDirection():
 		velocity.x = flying_speed
 	elif abs(velocity.x) != flying_speed && _animated_sprite.flip_h == true:
 		velocity.x = -flying_speed
-		
-		
-	
-#	if velocity.x > 0:
-#		_animated_sprite.flip_h = false
-#	elif velocity.x < 0:
-#		_animated_sprite.flip_h = true
-#	else: # if velocity is 0
-#		if _animated_sprite.flip_h == false: # sprite facing right
-#			velocity.x = flying_speed
-#		else:
-#			velocity.x = -flying_speed
+
+
+func _on_BeeHitBox_body_entered(body):
+	print(body)
+	body.velocity.y = -400 # Replace with function body
+	self._animated_sprite.play("hit")
+	_bee_animation_timer.start()
+
+
+func _on_BeeHitAnimationTimer_timeout():
+	_animated_sprite.play("default")
